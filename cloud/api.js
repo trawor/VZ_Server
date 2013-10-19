8ac2d267d18abcc66558c77c1dcf5b643a8bdd55
Travis <i@imi.im>
2013-10-19 20:59:16 +0800

    config

--- cloud/api.js
+++ cloud/api.js
@@ -1,4 +1,16 @@
-var config  = require('./cloud/config');
+var config  = = {
+    default_count:30,
+    weibo:{
+        appkey:'2858658895',
+        appsec:"9d97c1cce2893cbdcdc970f05bc55fe4",
+    },
+    channel_account:{
+        shuma:['2043408047','1761596064','1882458640','1841288857','3787475667'],
+        //muying:['1769267263','2813948131','3057011237','3263012774'],
+    },
+    block_account:['3170023534'],
+}
+
 var weibo   = require('./cloud/weibo');
 
 var Post = AV.Object.extend("Post");
