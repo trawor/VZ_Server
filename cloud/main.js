3d2dbb205099d38278e4cde27de1e6c80882ef30
Travis <i@imi.im>
2013-10-20 10:37:49 +0800

    api

--- cloud/main.js
+++ cloud/main.js
@@ -3,8 +3,9 @@ var api=require("cloud/api.js");
 var model=require("cloud/model.js");
 
 AV.Cloud.setInterval("refresh",1800, function() {
-    console.info(model);
     api.refresh.all();
 });
 
 console.info('VZ restart!');
+console.info(model);
+    
