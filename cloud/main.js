c91a2da90b12b659d67af35aa54eec1abc97104b
Travis <i@imi.im>
2013-10-20 10:41:57 +0800

    exports

--- cloud/main.js
+++ cloud/main.js
@@ -1,11 +1,11 @@
 var app=require("cloud/app.js");
 var api=require("cloud/api.js");
-var model=require("cloud/model.js");
+
 
 AV.Cloud.setInterval("refresh",1800, function() {
     api.refresh.all();
 });
 
 console.info('VZ restart!');
-console.info(model);
+
     
--- cloud/model.js
+++ cloud/model.js
@@ -1,6 +1,6 @@
 
 
-var textTool={
+exports.textTool={
     trim:function (text) {
         //删除@的多个微博账号
         var delReg=/@[^ $]+|我在(这里)?:http:\/\/[a-zA-Z./0-9]+|((麻烦)|([多]?谢[谢]?)).+[\s|,|，|了]/g;
@@ -39,20 +39,19 @@ var textTool={
 
 }
 
-module.exports={
-    default_count:30,
-    weibo:{
+exports.channel_account={
+    shuma:['2043408047','1761596064','1882458640','1841288857','3787475667'],
+    //muying:['1769267263','2813948131','3057011237','3263012774'],
+}
+
+exports.default_count=30;
+
+exports.weibo={
         appkey:'2858658895',
         appsec:"9d97c1cce2893cbdcdc970f05bc55fe4",
-    },
-    channel_account:{
-        shuma:['2043408047','1761596064','1882458640','1841288857','3787475667'],
-        //muying:['1769267263','2813948131','3057011237','3263012774'],
-    },
-    block_account:['3170023534'],
-
-    textTool:textTool,
 }
+    
+exports.block_account=['3170023534'];
 
 //价格case
 /*
