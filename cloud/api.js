f35267f1e5ef42550ea3f06530912c14deddee43
Travis <i@imi.im>
2013-10-20 10:36:57 +0800

    api

--- cloud/api.js
+++ cloud/api.js
@@ -1,4 +1,4 @@
-var model   = require('cloud/model');
+var model   = require('cloud/model.js');
 var weibo   = require('cloud/weibo.js');
 
 var Post = AV.Object.extend("Post");
--- cloud/main.js
+++ cloud/main.js
@@ -1,7 +1,9 @@
 var app=require("cloud/app.js");
 var api=require("cloud/api.js");
+var model=require("cloud/model.js");
 
 AV.Cloud.setInterval("refresh",1800, function() {
+    console.info(model);
     api.refresh.all();
 });
 
