a1b6d8123b98bf3bb17a277a9362ba2594ffedc0
Travis <travis@avos.com>
2013-10-21 13:30:50 +0800

    log

--- cloud/main.js
+++ cloud/main.js
@@ -2,7 +2,7 @@ var app=require("cloud/app.js");
 var api=require("cloud/api.js");
 
 
-AV.Cloud.setInterval("refresh",1800, function() {
+AV.Cloud.setInterval("weiboRefresh",1800, function() {
 	console.info('refresh ----------------');
     api.refresh.all();
 });
