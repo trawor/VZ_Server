371a599977371fbfcf495ed02bf8645a15f36942
Travis <travis@avos.com>
2013-10-21 13:21:29 +0800

    log

--- cloud/main.js
+++ cloud/main.js
@@ -3,9 +3,9 @@ var api=require("cloud/api.js");
 
 
 AV.Cloud.setInterval("refresh",1800, function() {
+	console.info('refresh ----------------');
     api.refresh.all();
 });
 
-console.info('VZ restart!');
 
     
