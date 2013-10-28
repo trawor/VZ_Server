786ffcfd76641b9389e8f49e084341224e4c5483
Travis <travis@avos.com>
2013-10-28 18:36:19 +0800

    分出测试环境生产环境

--- cloud/main.js
+++ cloud/main.js
@@ -2,10 +2,14 @@ var app=require("cloud/app.js");
 var api=require("cloud/api.js");
 
 
-AV.Cloud.setInterval("weiboRefresh",1800, function() {
-	console.info('refresh ----------------');
-    api.refresh.all();
-});
+if(__production){
+	AV.Cloud.setInterval("weiboRefresh",1800, function() {
+		console.info('refresh ----------------');
+	    api.refresh.all();
+	});
+}
+	
+
 
 
     
