092fdcd5b4fb798425ba26f68b05f1297c779b2b
Travis <travis@avos.com>
2013-11-01 16:03:23 +0800

    hook test 2

--- cloud/weibo.js
+++ cloud/weibo.js
@@ -3,7 +3,7 @@ var model=require("cloud/model.js");
 
 exports.fetchPosts=function (userid,from,callback) {
     from='0'; //强制重新刷
-
+    
     var count=20;
     var path="/2/statuses/user_timeline.json?source=4229079448&uid="+userid+"&since_id="+from+"&count="+count;
     var url="http://api.weibo.com"+path;
